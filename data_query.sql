fcc47989666681dc0f9b8a8060a64424abc29668
reinierandrew <reinierandrew@me.com>
2023-02-23 19:37:09 +1100

    final commit

--- data_query.sql
+++ data_query.sql
@@ -1,11 +1,11 @@
 -- check if there are 1000 records in campaign table (same as csv)
 select * from campaign
 
--- check if there are 1000 records in cotacts table (same as csv)
+-- check if there are 1000 records in contacts table (same as csv)
 select * from contacts
 
--- check if there are 9 records in cotacts table (same as csv)
+-- check if there are 9 records in category table (same as csv)
 select * from category
 
--- check if there are 24 records in cotacts table (same as csv)
+-- check if there are 24 records in sub_category table (same as csv)
 select * from sub_category
